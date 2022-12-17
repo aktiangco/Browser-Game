8b4ae0d22a2b5304104ab40faad7f57b74a82223
aktiangco <113158472+aktiangco@users.noreply.github.com>
2022-12-17 12:47:03 -0800

    fix  game area background

--- js/index.js
+++ js/index.js
@@ -2,31 +2,35 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const grid = 48; 
+const grid = 48; // 1 = 48px
 const gridGap = 10;
 
 // draw the game background
 // start zone
 ctx.fillStyle = 'gray';
-ctx.fillRect(0, 2 * grid, canvas.width, grid * 12);
+ctx.fillRect(0, grid, canvas.width, grid * 14);
 
-// road
+// road zone
 ctx.fillStyle = 'black';
-ctx.fillRect(0, grid, canvas.width, grid * 11.5);
+ctx.fillRect(0, grid, canvas.width, grid * 12);
 
-// water 
+
+// water zone 
 ctx.fillStyle = 'blue';
 ctx.fillRect(0, grid, canvas.width, grid * 6);
 
-// grass
+// grass zone
 ctx.fillStyle = 'green';
 ctx.fillRect(0, 7 * grid, canvas.width, grid);
   
- // end bank
- ctx.fillStyle = 'brown';
- ctx.fillRect(0, grid, canvas.width, 10);
- ctx.fillRect(0, grid, 5, grid);
- ctx.fillRect(canvas.width - 4, grid, 4, grid);
- for (let i = 0; i < 8; i++) {
+ // end zone
+ctx.fillStyle = 'brown';
+ctx.fillRect(0, grid, canvas.width, grid * -1);
+ctx.fillRect(0, grid, canvas.width, 0);
+ctx.fillRect(0, grid, 5, grid);
+ctx.fillRect(canvas.width - 4, grid, 4, grid);
+ for (let i = 0; i < 5; i++) {
    ctx.fillRect(grid + grid * 4 * i, grid, grid * 2, grid);
  }
+
+
