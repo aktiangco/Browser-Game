fb5389f77311d5a81d495f2c84c95cf24a61cbec
aktiangco <113158472+aktiangco@users.noreply.github.com>
2022-12-22 10:06:51 -1000

    revising H1 tag and P tag in HTML

--- js/index.js
+++ js/index.js
@@ -9,11 +9,12 @@ const checkHit = setInterval(() => {
     let blockLeft = parseInt(window.getComputedStyle(block1).getPropertyValue("left"));
     if (blockLeft < 50 && blockLeft > 20 && characterTop >= 230) {
        // to make objects display "none" 
-        // DRY how to simplify it
+        // DRY how to simplify it //
         character.style.display = "none";
         block1.style.display = "none";
         block2.style.display = "none";
         scoreElement.style.display = "none"; // hitElement codes in score.js
+        ///////////////////////////
         swal("you lost", "Try Again?", "error"); // Using sweetAlert vs ugly alert
     }
 }, 10);
