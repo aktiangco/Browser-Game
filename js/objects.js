7fe89731058f1006f0326a52586466766f167360
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-01-10 20:33:44 -0800

    adjusted the buttons

---
+++ js/objects.js
@@ -0,0 +1,13 @@
+
+    
+    
+    // // Randomize safe zones
+    // const randomObject = () => {
+    //     let random = -(Math.random() * 60);
+    //     rocket.style.top = random + "px";
+    //     counter++;
+    //     counterElement.textContent = `${counter}`
+    // }
+    // rocket.addEventListener('animationiteration', randomObject);
+
+ 
